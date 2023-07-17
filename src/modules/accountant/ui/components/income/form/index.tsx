89ab5a18730ca6c03f60ee0ba4bf1ed6291f282e
afdavletkulov <afdavletkulov@rttv.ru>
2023-07-17 11:21:13 -0300

    test-array-fields

--- src/modules/accountant/ui/components/income/form/index.tsx
+++ src/modules/accountant/ui/components/income/form/index.tsx
@@ -2,17 +2,55 @@ import { memo } from 'react'
 import type { DatePickerProps } from 'antd'
 import { DatePicker } from 'antd'
 import { Col, Row } from 'antd'
-import { useForm, SubmitHandler } from 'react-hook-form'
+import { useForm, SubmitHandler, useFieldArray, Control } from 'react-hook-form'
 import { Input } from 'antd'
 import { InputForm } from 'shared/ui/formBlocks'
 import { Button } from 'antd'
 
 const { TextArea } = Input
 
+const test = [
+  {
+    title: 'hell',
+    name: 'tyu',
+  },
+  {
+    title: 'zed',
+    name: 'tyu',
+  },
+]
+
 type Inputs = {
   category: string
   amount: number
-  test?: string
+  test?: any[]
+}
+
+const TestArray = ({
+  register,
+  control,
+}: {
+  register: any
+  control: Control<Inputs>
+}) => {
+  const { fields, append, prepend, remove, swap, move, insert } = useFieldArray(
+    {
+      control,
+      name: 'test',
+    }
+  )
+
+  console.log('fields', fields)
+  return (
+    <>
+      {fields.map((field, index) => (
+        <input
+          key={field.id} // important to include key with field's id
+          {...register(`test.${index}.value`)}
+        />
+      ))}
+    </>
+  )
 }
 
 const EditFormModal = memo(() => {
@@ -20,13 +58,12 @@ const EditFormModal = memo(() => {
     console.log(date, dateString)
   }
 
-  const {
-    control,
-    handleSubmit,
-  } = useForm<Inputs>({
+  //TODO: если получаем объект можно проконтролировать тут 
+  const { control, handleSubmit, register } = useForm<Inputs>({
     defaultValues: {
       category: 'hello',
       amount: 123,
+      test: test,
     },
   })
 
@@ -38,6 +75,9 @@ const EditFormModal = memo(() => {
     <form>
       <div className="modal__input">
         <InputForm control={control} name="category" placeholder="Категория" />
+      </div>
+      <div>
+        <TestArray register={register} control={control} />
       </div>
       <Row className="modal__row" gutter={[16, 0]}>
         <Col flex={1}>
