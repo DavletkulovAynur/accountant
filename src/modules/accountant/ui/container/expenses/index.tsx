80ecee61df29b6d7e0019913de62f21fd2a9b433
afdavletkulov <afdavletkulov@rttv.ru>
2023-08-17 11:45:15 -0300

    new-file-structure

---
+++ src/modules/accountant/ui/container/expenses/index.tsx
@@ -0,0 +1,11 @@
+import React, { memo } from 'react'
+
+const Expenses: React.FC = memo(() => {
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  )
+})
+
+export { Expenses }
--- src/modules/accountant/ui/index.tsx
+++ src/modules/accountant/ui/index.tsx
@@ -1,71 +1,9 @@
-import React, { memo, useState } from 'react'
+import React, { memo } from 'react'
 
-import { Layout, theme } from 'antd'
-import { Tabs } from 'antd'
-import type { TabsProps } from 'antd'
-import { Income } from './components/income'
-import { PlusOutlined } from '@ant-design/icons'
-import { CreateOperationModal } from './components/createOperation'
-
-const { Content } = Layout
-
-const items: TabsProps['items'] = [
-  {
-    key: '1',
-    label: `Расходы`,
-    children: 'costs',
-  },
-  {
-    key: '2',
-    label: `Доходы`,
-    children: <Income />,
-  },
-]
+import { AccountantContainer } from './container'
 
 const Accountant: React.FC = memo(() => {
-  const [isOpenCreateOperationModal, setIsOpenCreateOperationModal] =
-    useState(false)
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken()
-
-  const onChange = (key: string) => {
-    console.log(key)
-  }
-
-  const openCreateOperationModal = () => {
-    setIsOpenCreateOperationModal(true)
-  }
-
-  const closeCreateOperationModal = () => {
-    setIsOpenCreateOperationModal(false)
-  }
-
-  return (
-    <Content
-      style={{
-        margin: '24px 16px',
-        padding: 24,
-        minHeight: 280,
-        background: colorBgContainer,
-      }}
-    >
-      <>
-        <div>
-        <PlusOutlined
-          style={{ fontSize: '24px' }}
-          onClick={openCreateOperationModal}
-        />
-        </div>
-       
-        <Tabs defaultActiveKey="1" items={items} onChange={onChange} />
-        <CreateOperationModal
-          isOpenCreateOperationModal={isOpenCreateOperationModal}
-          closeModal={closeCreateOperationModal}
-        />
-      </>
-    </Content>
-  )
+  return <AccountantContainer />
 })
 
 export { Accountant }
